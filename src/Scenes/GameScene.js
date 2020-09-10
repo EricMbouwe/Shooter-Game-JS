e296595703140ea03bb57b37e5a5e98c2ed4de07
ericmbouwe <ericmbouwe@yahoo.fr>
2020-09-10 23:56:33 +0100

    added anilations to gamescene

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -38,6 +38,43 @@ export default class GameScene extends Phaser.Scene {
   }
 
   create() {
+    // create our animations
     this.add.image(400, 300, 'logo');
+    this.anims.create({
+      key: "sprEnemy0",
+      frames: this.anims.generateFrameNumbers("sprEnemy0"),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "sprEnemy2",
+      frames: this.anims.generateFrameNumbers("sprEnemy2"),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "sprExplosion",
+      frames: this.anims.generateFrameNumbers("sprExplosion"),
+      frameRate: 20,
+      repeat: 0
+    });
+
+    this.anims.create({
+      key: "sprPlayer",
+      frames: this.anims.generateFrameNumbers("sprPlayer"),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    // add sound to variable objects
+    this.sfx = {
+      explosions: [
+        this.sound.add("sndExplode0"),
+        this.sound.add("sndExplode1")
+      ],
+      laser: this.sound.add("sndLaser")
+    };
   }
 }
