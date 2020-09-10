bf9210d8fcebe5cbe8bf6aa551bd8cb4a0c84f3f
ericmbouwe <ericmbouwe@yahoo.fr>
2020-09-10 23:19:21 +0100

    added physics and gravity to config

--- src/Config/config.js
+++ src/Config/config.js
@@ -5,4 +5,14 @@ export default {
   parent: 'phaser-example',
   width: 800,
   height: 600,
+  backgroundColor: "black",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 0, y: 0 }
+    }
+  },
+  pixelArt: true,
+  roundPixels: true
 };
+
