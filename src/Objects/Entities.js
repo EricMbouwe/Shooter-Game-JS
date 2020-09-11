ff89acf342d583328eca1bb16fda7ed05646a9f8
ericmbouwe <ericmbouwe@yahoo.fr>
2020-09-11 19:41:02 +0100

    added enemies fattack function

--- src/Objects/Entities.js
+++ src/Objects/Entities.js
@@ -40,22 +40,75 @@ class Player extends Entity {
   }
 }
 
+class EnemyLaser extends Entity {
+  constructor(scene, x, y) {
+    super(scene, x, y, "sprLaserEnemy0");
+    this.body.velocity.y = 200;
+  }
+}
+
 class ChaserShip extends Entity {
   constructor(scene, x, y) {
     super(scene, x, y, "sprEnemy1", "ChaserShip");
+    this.body.velocity.y = Phaser.Math.Between(50, 100);
+  }
+
+  update() {
+    if (!this.getData("isDead") && this.scene.player) {
+      if (
+        Phaser.Math.Distance.Between(
+          this.x,
+          this.y,
+          this.scene.player.x,
+          this.scene.player.y
+        ) < 320
+      ) {
+        this.state = this.states.CHASE;
+      }
+
+      if (this.state == this.states.CHASE) {
+        var dx = this.scene.player.x - this.x;
+        var dy = this.scene.player.y - this.y;
+
+        var angle = Math.atan2(dy, dx);
+
+        var speed = 100;
+        this.body.setVelocity(Math.cos(angle) * speed, Math.sin(angle) * speed);
+      }
+    }
   }
 }
 
 class GunShip extends Entity {
   constructor(scene, x, y) {
     super(scene, x, y, "sprEnemy0", "GunShip");
+    this.body.velocity.y = Phaser.Math.Between(50, 100);
+    this.shootTimer = this.scene.time.addEvent({
+      delay: 1000,
+      callback: function () {
+        var laser = new EnemyLaser(this.scene, this.x, this.y);
+        laser.setScale(this.scaleX);
+        this.scene.enemyLasers.add(laser);
+      },
+      callbackScope: this,
+      loop: true,
+    });
     this.play("sprEnemy0");
+  }
+
+  onDestroy() {
+    if (this.shootTimer !== undefined) {
+      if (this.shootTimer) {
+        this.shootTimer.remove(false);
+      }
+    }
   }
 }
 
 class CarrierShip extends Entity {
   constructor(scene, x, y) {
     super(scene, x, y, "sprEnemy2", "CarrierShip");
+    this.body.velocity.y = Phaser.Math.Between(50, 100);
     this.play("sprEnemy2");
   }
 }
--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,34 +1,34 @@
-import 'phaser';
+import "phaser";
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
-    super('Game');
+    super("Game");
   }
 
   preload() {
     // load images
-    this.load.image('logo', 'assets/logo.png');
+    this.load.image("logo", "assets/logo.png");
     this.load.image("sprBg0", "assets/sprBg0.png");
     this.load.image("sprBg0", "assets/sprBg0.png");
     this.load.image("sprBg1", "assets/sprBg1.png");
     this.load.spritesheet("sprExplosion", "assets/sprExplosion.png", {
       frameWidth: 32,
-      frameHeight: 32
+      frameHeight: 32,
     });
     this.load.spritesheet("sprEnemy0", "assets/sprEnemy0.png", {
       frameWidth: 16,
-      frameHeight: 16
+      frameHeight: 16,
     });
     this.load.image("sprEnemy1", "assets/sprEnemy1.png");
     this.load.spritesheet("sprEnemy2", "assets/sprEnemy2.png", {
       frameWidth: 16,
-      frameHeight: 16
+      frameHeight: 16,
     });
     this.load.image("sprLaserEnemy0", "assets/sprLaserEnemy0.png");
     this.load.image("sprLaserPlayer", "assets/sprLaserPlayer.png");
     this.load.spritesheet("sprPlayer", "assets/sprPlayer.png", {
       frameWidth: 16,
-      frameHeight: 16
+      frameHeight: 16,
     });
 
     //load sounds
@@ -39,42 +39,42 @@ export default class GameScene extends Phaser.Scene {
 
   create() {
     // create our animations
-    this.add.image(400, 300, 'logo');
+    this.add.image(400, 300, "logo");
     this.anims.create({
       key: "sprEnemy0",
       frames: this.anims.generateFrameNumbers("sprEnemy0"),
       frameRate: 20,
-      repeat: -1
+      repeat: -1,
     });
 
     this.anims.create({
       key: "sprEnemy2",
       frames: this.anims.generateFrameNumbers("sprEnemy2"),
       frameRate: 20,
-      repeat: -1
+      repeat: -1,
     });
 
     this.anims.create({
       key: "sprExplosion",
       frames: this.anims.generateFrameNumbers("sprExplosion"),
       frameRate: 20,
-      repeat: 0
+      repeat: 0,
     });
 
     this.anims.create({
       key: "sprPlayer",
       frames: this.anims.generateFrameNumbers("sprPlayer"),
       frameRate: 20,
-      repeat: -1
+      repeat: -1,
     });
 
     // add sound to variable objects
     this.sfx = {
       explosions: [
         this.sound.add("sndExplode0"),
-        this.sound.add("sndExplode1")
+        this.sound.add("sndExplode1"),
       ],
-      laser: this.sound.add("sndLaser")
+      laser: this.sound.add("sndLaser"),
     };
 
     // create a player
@@ -90,7 +90,29 @@ export default class GameScene extends Phaser.Scene {
     this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
     this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    this.keySpace = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
+
+    // create a Group to hold our enemies, the lasers shot by enemies, and the lasers shot by the player
+    this.enemies = this.add.group();
+    this.enemyLasers = this.add.group();
+    this.playerLasers = this.add.group();
+
+    // create an event (it will act as a timer) which will spawn our enemies
+    this.time.addEvent({
+      delay: 100,
+      callback: function () {
+        var enemy = new GunShip(
+          this,
+          Phaser.Math.Between(0, this.game.config.width),
+          0
+        );
+        this.enemies.add(enemy);
+      },
+      callbackScope: this,
+      loop: true,
+    });
   }
 
   update() {
@@ -98,15 +120,13 @@ export default class GameScene extends Phaser.Scene {
 
     if (this.keyW.isDown) {
       this.player.moveUp();
-    }
-    else if (this.keyS.isDown) {
+    } else if (this.keyS.isDown) {
       this.player.moveDown();
     }
 
     if (this.keyA.isDown) {
       this.player.moveLeft();
-    }
-    else if (this.keyD.isDown) {
+    } else if (this.keyD.isDown) {
       this.player.moveRight();
     }
   }
